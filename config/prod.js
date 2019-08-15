3bb307a6e438ea8b56b8cadd59999c41891e827b
PedroRocha-1150936 <1150936@isep.ipp.pt>
2019-08-15 19:44:30 +0100

    Setup prod and env

---
+++ config/prod.js
@@ -0,0 +1,14 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
+};
+
+/** module.exports = {
+    googleClientID: '86230471523-rbg69b0cugbkm96lovd6hraejjg88iml.apps.googleusercontent.com',
+    googleClientSecret: 'wqrayhZxg8ilrSq_vfalgJnP',
+    mongoURI: 'mongodb+srv://pedro:pedro@app-database-i0i9t.mongodb.net/prod-database?retryWrites=true&w=majority',
+    cookieKey: 'cxzczxuycizuxyczi'
+};
+*/
